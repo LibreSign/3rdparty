1f2dacb511c426d6b6ab202200ef87a733f66cda
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-06-16 19:59:26 -0300

    build: update composer boilerplate to Composer 2.10.1 format
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- composer/autoload.php
+++ composer/autoload.php
@@ -15,7 +15,7 @@ if (\PHP_VERSION_ID < 50600) {
             echo $err;
         }
     }
-    \trigger_error($err, \E_USER_ERROR);
+    throw new \RuntimeException($err);
 }
 require_once __DIR__ . '/composer/autoload_real.php';
 return ComposerAutoloaderInitLibresign::getLoader();
