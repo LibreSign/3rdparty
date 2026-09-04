6f5ccabea5e6fc21e92f439470f249da87f02336
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-09-04 13:03:05 -0300

    Update PDF signature validator to v0.5.0
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ composer/libresign/pdf-signature-validator/src/Model/TimestampToken.php
@@ -0,0 +1,17 @@
+<?php
+
+// SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+declare (strict_types=1);
+namespace OCA\Libresign\Vendor\LibreSign\PdfSignatureValidator\Model;
+
+/** @internal */
+final class TimestampToken
+{
+    /**
+     * @param array<string, string> $certificateSubject
+     */
+    public function __construct(public readonly ?\DateTimeImmutable $generatedAt, public readonly ?string $policyOid, public readonly ?string $serialNumber, public readonly array $certificateSubject)
+    {
+    }
+}
